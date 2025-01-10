e38704df514022e7b12a314e628c186acf172895
MD Shakib Ahmed <shakibahmed.528874@gmail.com>
2025-01-10 18:08:43 +0600

    added full_name

--- doctor/models.py
+++ doctor/models.py
@@ -24,16 +24,24 @@ class AvailableTime(models.Model):
 
     def __str__(self):
         return f"{self.name}"
-    
+
 
 class Doctor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='doctor')
+    full_name = models.CharField(max_length=100, blank=True, null=True)
     image = models.ImageField(upload_to="doctor/images/")
     designation = models.ManyToManyField(Designation, related_name='designation')
     specialization = models.ManyToManyField(Specialization, related_name='specialization')
     available_time = models.ManyToManyField(AvailableTime, related_name='available_time')
     fee = models.IntegerField()
     meet_link = models.URLField(max_length=100)
+
+    def save(self, *args, **kwargs):
+        if self.user:
+            f_name = self.user.first_name
+            l_name = self.user.last_name
+            self.full_name = f"{f_name} {l_name}"
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"Dr. {self.user.first_name} {self.user.last_name}"
--- patient/models.py
+++ patient/models.py
@@ -3,8 +3,16 @@ from django.contrib.auth.models import User
 
 class Patient(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='patient')
+    full_name = models.CharField(max_length=100, blank=True, null=True)
     image = models.ImageField(upload_to="patient/images/")
     mobile = models.CharField(max_length=20)
+
+    def save(self, *args, **kwargs):
+        if self.user:
+            f_name = self.user.first_name
+            l_name = self.user.last_name
+            self.full_name = f"{f_name} {l_name}"
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name}"
